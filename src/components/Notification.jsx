dec0705b7722e7530052e035d3acc00232c42837
ryangholland <rghcode@gmail.com>
2023-08-09 15:27:53 -0400

    Style notifications div

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,37 +1,36 @@
-function Notification({ id, name, action, target, time, message, picture, read }) {
+import pic from "../assets/images/angela.webp";
+
+function Notification({
+  id,
+  name,
+  action,
+  target,
+  time,
+  message,
+  picture,
+  read,
+}) {
   return (
     <>
-      <div className="notification-div">
+      <div className={`notification-div ${!read && 'unread-bg'}`} >
         <div className="avatar">
-          <img src="#" alt="#" />
+          <img src={pic} alt="#" />
         </div>
         <div className="notification-content">
-          <p>
+          <p className="notification-text">
             <span className="notification-name">{name}</span> {action}{" "}
-            <span className="notification-target">{target}</span>{" "}
+            {target != "" && <span className="notification-target" >{target}</span>}{" "}
             {!read && <span className="unread-dot"></span>}
           </p>
-          <p>{time} ago</p>
-          {message && <div className="message-div">{message}</div>}
+          <p className="notification-time">{time} ago</p>
         </div>
         {picture && <div>{picture}</div>}
       </div>
+      {message && <div className="message-div">{message}</div>}
     </>
   );
 }
 
 export default Notification;
 
-/*
 
-Dot class example
-
-.dot {
-  height: 25px;
-  width: 25px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-}
-
-*/
