e98d8ca304fb952aa484effda8d84d9fe9a64f94
ryangholland <rghcode@gmail.com>
2023-08-09 21:05:33 -0400

    Style active states

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -14,21 +14,21 @@ function Notification({
     <>
       <div
         className={`notification-div ${!read && "unread-bg"}`}
-        onClick={() => changeReadStatus(id)}
+        onClick={() => changeReadStatus(id)} 
       >
         <div className="avatar">
           <img src={avatar} alt="#" />
         </div>
         <div className="notification-content">
           <p className="notification-text">
-            <span className="notification-name">{name}</span> {action}{" "}
+            <a className="notification-name" href="#">{name}</a> {action}{" "}
             {target != "" && (
-              <span className="notification-target">{target}</span>
+              <a className="notification-target" href="#">{target}</a>
             )}{" "}
             {!read && <span className="unread-dot"></span>}
           </p>
           <p className="notification-time">{time} ago</p>
-          {message && <div className="message-div">{message}</div>}
+          {message && <div className="message-div"><a href="#">{message}</a></div>}
         </div>
         {picture && <img className="comment-pic" src={picture} alt="#" />}
       </div>
